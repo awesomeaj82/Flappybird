a30e31b2b028a2a3dd6378759b7460014fcb9051
awesomeaj82 <awesomeaj82@gmail.com>
2017-02-20 22:56:59 -0600

    falling object

---
+++ unity/FlappyBird/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Time.deltaTime * 1;
+        transform.Translate(0, -translation, 0);
+        if (Input.GetKeyDown("space"))
+         transform.Translate(0, 3, 0); ;
+    }
+}
